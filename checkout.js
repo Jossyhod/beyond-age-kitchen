0881f1ea9653d5c3504a02017478b51540323de6
Joseph <ikeanyi_joseph@yahoo.com>
2023-12-02 22:18:15 +0100

    initial Update

---
+++ checkout.js
@@ -0,0 +1,5 @@
+let checkoutOrder2 = document.querySelector('.checkoutOrder2').value = sessionStorage.getItem('orderToPass');
+let checkoutName2 = document.querySelector('.checkoutName2').value = localStorage.getItem('formName');
+let checkoutPrice2 = document.querySelector('.checkoutPrice2').value = sessionStorage.getItem('priceToPass');
+let checkoutMobile2 = document.querySelector('.checkoutMobile2').value = localStorage.getItem('formNumber');
+let checkoutEmail2 = document.querySelector('.checkoutEmail2').value = localStorage.getItem('formEmail');
